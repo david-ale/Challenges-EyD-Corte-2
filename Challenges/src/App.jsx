220ba11e5aeb0d637ae5d85d1b6b185b94e97464
david-ale <david_ale.sanchez@uao.edu.co>
2023-09-20 16:59:54 -0500

    amen

---
+++ Challenges/src/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hola Mundo!</h1>
+      <h2>Bienvenido</h2>
+    </>
+  )
+}
+
+export default App
---
+++ Challenges/src/FirstApp.jsx
@@ -0,0 +1,9 @@
+
+export const FirstApp = ({title}) =>{
+    return (
+        <>
+            <h1> {title} </h1>
+            <span> 10 </span>
+        </>
+    )
+}
