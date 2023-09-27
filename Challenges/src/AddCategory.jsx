286cfc4c91ffc51899b333caaaae5b538acbe562
david-ale <david_ale.sanchez@uao.edu.co>
2023-09-27 16:42:58 -0500

    si

---
+++ Challenges/src/AddCategory.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const AddCategory = ({onAddCategory}) => {
+    const[inputValue,setInputValue] = useState('...')
+
+    const onInputChange = (evt) =>{
+        setInputValue( )
+    }
+
+
+} 
