236dd6c9993dbc9283e0c30d8beb96d2fdb0d1eb
david-ale <david_ale.sanchez@uao.edu.co>
2023-10-11 14:57:39 -0500

    terminados challenges 2do corte

--- Challenges/src/main.jsx
+++ Challenges/src/main.jsx
@@ -6,7 +6,7 @@ import { SecondApp } from './Challenge02 - buttons/SecondApp.jsx'
 import { GifExpertApp } from './Challenge03 - categories/ComponentApp.jsx'
 import { GifGrid } from './Components/./Challenge04 - gifs/GifGrid'
 import {Counter} from "./Components/Counter.jsx"
-import { MultipleCustomHook } from './Components/Challenge08 - breakingbad/MultipleCustomHook.jsx'
+import {MultipleCustomHook} from "./Components/Challenge07 - breakingbad/MultipleCustomHook"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
